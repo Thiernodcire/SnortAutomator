1a163ffc53cd2e6b0c94b939bfd3607fe318214f
Thierno Diallo <thiernodcire@gmail.com>
2021-07-14 18:53:32 -0400

    'Combined the two codes together'

--- SnortAutomator_GUI.py
+++ SnortAutomator_GUI.py
@@ -5,7 +5,10 @@ from tkinter import font
 from tkinter.constants import GROOVE, SUNKEN
 import tkinter.messagebox as tkMessageBox
 from typing import Text
-import SnortAutomator_backend as sab
+import pyshark as py
+src_dictionary = {}
+dst_dictionary = {}
+snort_rule_list = []
 
 #Set variables for the characteristics of GUI interface
 Height = 500
@@ -40,13 +43,58 @@ def baseline_options():
     instrc.pack()
     entry = tk.Text(top,relief=GROOVE,borderwidth=2)
     entry.place(relx=0.2, rely=0.1, relwidth= .5, relheight=.71)
-    start_button = tk.Button(top, text='Create Snort Rules')
-    start_button.place(relx= .25, rely= .8, relwidth= .4, relheight=.09, command=lambda:sab.compare_traffic(entry.get()))
-    exit_button = tk.Button(top, text='Exit Program', command=top.destroy)
+    start_button = tk.Button(top, text='Create Snort Rules',command= lambda:compare_traffic(entry.get('1.0', 'end')))
+    start_button.place(relx= .25, rely= .8, relwidth= .4, relheight=.09)
+    exit_button = tk.Button(top, text='Exit Program',command=top.destroy)
     exit_button.place(relx= .3, rely= .9, relheight= .09 , relwidth=.3)
 #Create a fucntion for the save button
 def save_file():
     reponse = tkMessageBox.showerror('Error', "There are no rules to save")
+
+def errors():
+        response = tkMessageBox.showerror('You entered the IP addresses wrong') 
+def pcap_capture(pcap,):
+    cap = py.FileCapture(pcap)
+    for pack in cap:
+        try:
+            ip_src = pack.ip.src
+            ip_dst = pack.ip.dst
+            src_port = pack.tcp.srcport
+            dst_port = pack.tcp.dstport
+        except:
+            arp_traffic = pack
+        src_dictionary[ip_src] = src_port
+        dst_dictionary[ip_dst] = dst_port
+def live_capture(timeout_n,interface_c):
+    capture = py.LiveCapture(interface=interface_c, display_filter="tcp")
+    capture.sniff(timeout=timeout_n)
+    for pack in capture:
+        try:
+            ip_src = pack.ip.src
+            ip_dst = pack.ip.dst
+            src_port = pack.tcp.srcport
+            dst_port = pack.tcp.dstport
+        except:
+            arp_traffic = pack
+        src_dictionary[ip_src] = src_port
+        dst_dictionary[ip_dst] = dst_port
+    return 'Done' 
+def compare_traffic(whitelist_ip):
+    if whitelist_ip != '' and whitelist_ip == '[0-9]{1,3}\.){3}[0-9]{1,3}':
+        striped_whitelist_ip = whitelist_ip.strip(' ')
+        for idx, ip in enumerate(src_dictionary.keys()):
+            if ip not in striped_whitelist_ip:
+                snort_rule = 'alert tcp {bad_src_ip} {bad_src_port} -> {bad_dst_ip} {bad_dst_port}'
+                snort_rule_list.append(snort_rule.format(bad_src_ip=ip,bad_src_port=src_dictionary[ip],bad_dst_ip=dst_dictionary.keys()[idx],bad_dst_port=dst_dictionary.values()[idx]))
+    else:
+        return sag.errors()
+    rule_generator(snort_rule_list)
+
+def rule_generator(rules):
+    print('Here, are your rules')
+    for rules in snort_rule_list:
+        print(rules)
+
 #Create the root base for the GUI
 root = tk.Tk()
 
